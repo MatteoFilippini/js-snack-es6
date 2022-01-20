f6fb5320ab3bab4f17b77b8585a7aa4299f94827
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-20 14:52:05 +0100

    created random function

--- js/script2.js
+++ js/script2.js
@@ -10,12 +10,22 @@ Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengo
 // creo l Array
 // oggetto{nome,punti fatti=0,falli subiti=0}
 // funzione numero random
+// assegon un numero random alle proprieta punti-fatti e falli-subiti
 
 
 const squadre = [
-    { nome: 'Inter', 'punti subiti': 0, 'falli subiti': 0 },
-    { nome: 'Milan', 'punti subiti': 0, 'falli subiti': 0 },
-    { nome: 'juve', 'punti subiti': 0, 'falli subiti': 0 }
+    { nome: 'Inter', 'punti fatti': 0, 'falli subiti': 0 },
+    { nome: 'Milan', 'punti fatti': 0, 'falli subiti': 0 },
+    { nome: 'juve', 'punti fatti': 0, 'falli subiti': 0 }
 ]
 
-console.log(squadre)
+const getRandomNumer = () => {
+    const numRandom = Math.floor(Math.random() * 5);
+
+    return numRandom;
+}
+
+
+
+
+console.log(getRandomNumer());
