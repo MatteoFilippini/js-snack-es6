07f8afc20103466d4127b4e98175919ff06f6ede
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-20 14:47:52 +0100

    cretaed array of objects

---
+++ js/script2.js
@@ -0,0 +1,21 @@
+/*
+Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+// creo l Array
+// oggetto{nome,punti fatti=0,falli subiti=0}
+// funzione numero random
+
+
+const squadre = [
+    { nome: 'Inter', 'punti subiti': 0, 'falli subiti': 0 },
+    { nome: 'Milan', 'punti subiti': 0, 'falli subiti': 0 },
+    { nome: 'juve', 'punti subiti': 0, 'falli subiti': 0 }
+]
+
+console.log(squadre)
