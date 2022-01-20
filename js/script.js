957bf9e80a4ce45e86aaeb9fc100726778b9fa92
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-20 14:32:34 +0100

    created road map

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare ain console la bici con peso minore utilizzando destructuring e template literal
+*/
+
+// creo un array di oggetti
+// oggetti {nome,peso}
+// prendo la proprieta peso di ogni oggetto
+//     -le confronto e trovo la minore
+// stampo
