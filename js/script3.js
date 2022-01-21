ee259810c7a9ce0836e9d67bec3a26a9e521f506
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-21 14:25:23 +0100

    created array

--- js/script3.js
+++ js/script3.js
@@ -3,3 +3,19 @@ Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più p
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array visti oggi in classe.
 */
+
+
+
+const names = [
+    { nome: 'Matteo' },
+    { nome: 'Marco' },
+    { nome: 'Mariano' },
+    { nome: 'Salvatore' },
+    { nome: 'Luca' }
+]
+
+console.log(names);
+
+const b = names.filter(nomi => nomi.nome === 'Luca');
+
+console.log(b)
